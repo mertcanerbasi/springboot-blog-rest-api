7c34bcae8e2abaa093bb2e55667d1f339b9f0cef
Mertcan ERBAŞI <mertcanerbasiemk@gmail.com>
2022-08-02 02:50:50 -0700

    create-comment-model

---
+++ src/main/java/com/springboot/blog/model/Comment.java
@@ -0,0 +1,31 @@
+package com.springboot.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private long id;
+
+    private String name;
+    private String email;
+    private String messageBody;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id",nullable = false)
+    private Post post;
+}
